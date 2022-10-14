dd8114ec65a7e72d69d5915d1882f7a7117447b9
Francis Chartrand <chartrandf@users.noreply.github.com>
2022-10-14 09:23:41 -0400

    feat: add selected state to reactions

--- src/MessageReactions.jsx
+++ src/MessageReactions.jsx
@@ -1,19 +1,29 @@
 import styles from './MessageReactions.module.scss';
 
 export default (props) => {
-  const combinedReactions = props?.reactions?.reduce((acc, { emoji }) => {
-    const count = acc?.[emoji] || 0;
-
-    return {
-      ...acc,
-      [emoji]: count + 1,
-    }
-  }, {})
+  const currentUserUuid = localStorage.getItem('currentUserUuid')
+  const combinedReactions = props?.reactions?.reduce((acc, { emoji, user_uuid }) => ({
+    ...acc,
+    [emoji]: {
+      count: (acc?.[emoji]?.count || 0) + 1,
+      selected: acc?.[emoji]?.selected || currentUserUuid === user_uuid,
+    },
+  }), {})
 
 
   return (
     <p className={styles.roomMessageReaction}>
-      {  Object.keys(combinedReactions).map(emoji => <span data-count={ combinedReactions[emoji] } className="message-reaction">{ emoji }</span>) }
+      {
+        Object.keys(combinedReactions).map(emoji => (
+          <span
+            data-count={ combinedReactions[emoji].count }
+            className="message-reaction"
+            classList={{ selected: combinedReactions[emoji].selected }}
+          >
+            { emoji }
+          </span>
+        ))
+      }
     </p>
   )
 }
